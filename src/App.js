bab681ef2c802ad6d82a021d2ba191beef6ee9ec
Yucohny <yucohny@163.com>
2022-01-28 22:21:31 +0800

    增加 footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import Header from "./Components/Header"
 import ContentBox from "./Components/ContentBox"
+import Footer from "./Components/Footer"
 
 export default class App extends Component {
     render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
             <>
                 <Header/>
                 <ContentBox/>
+                <Footer/>
             </>
         )
     }

657d1b108941471fbe3dc453f84eeec903f269ee
Yucohny <yucohny@163.com>
2022-01-28 22:02:50 +0800

    错误上传

--- src/Components/ContentRightBar/index.js
+++ src/Components/ContentRightBar/index.js
@@ -1,11 +1,22 @@
 import React, {Component} from "react"
+import ReactMarkdownHeading from 'react-markdown-heading'
 import './index.css'
+const markdown = '## h2\n### h3\n#### h4\n### h3\n# h1\n### h3'
 
 export default class ContentRightBar extends Component {
     render() {
         return (
             <div className="ContentRightBar">
-                目录<br/>等待更新
+                <ReactMarkdownHeading
+                    markdown={markdown}
+                    hyperlink={true}
+                    onChangeHeading={(headingList) => {
+                        console.log(headingList)
+                    }}
+                    liClassName="li"
+                    activeAnchorClassName="activeAnchor"
+                />
+                {/*目录<br/>等待更新*/}
             </div>
         )
     }

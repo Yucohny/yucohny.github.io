8c2cb979645ec60f5bd3bf27caeb882c89d8ceb7
Yucohny <yucohny@163.com>
2022-05-29 01:43:09 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React, { Component } from "react"
-import Header from "./Components/Header"
-import ContentBox from "./Components/ContentBox"
-import Footer from "./Components/Footer"
+import React from 'react'
+import 'antd/dist/antd.css'
 
-export default class App extends Component {
+import { Table } from 'antd';
+const data = [];
+for (let i = 0; i < 46; i++) {
+    data.push({
+        key: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`,
+    });
+}
+
+class App extends React.Component {
+    state = {
+        selectedRowKeys: [], // Check here to configure the default column
+    };
+
+    onSelectChange = selectedRowKeys => {
+        console.log('selectedRowKeys changed: ', selectedRowKeys);
+        this.setState({ selectedRowKeys });
+    };
+
     render() {
-        return (
-            <>
-                <Header/>
-                <ContentBox/>
-                <Footer/>
-            </>
-        )
+        const { selectedRowKeys } = this.state;
+        const rowSelection = {
+            selectedRowKeys,
+            onChange: this.onSelectChange
+        };
+        return <Table rowSelection={{type: 'checkbox'}} dataSource={data}>
+            <Table.Column title="name" dataIndex="name"/>
+            <Table.Column title="age" dataIndex="age"/>
+            <Table.Column title="address" dataIndex="address"/>
+        </Table>;
     }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {HashRouter} from "react-router-dom";
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <HashRouter>
-            <App/>
-        </HashRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
